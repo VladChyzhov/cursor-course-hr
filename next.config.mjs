99cb72c874620b9c632c3fe0f2264ff75e051f61
Vlad Chyzhov <mmmvch@gmail.com>
2025-07-14 13:07:03 +0200

    fix: исправил авторизацию и страницу пользователей

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,10 @@ const nextConfig = {
   images: {
     domains: ['lh3.googleusercontent.com'],
   },
+  experimental: {
+    appDir: true, // Включаем поддержку App Router
+  },
 };
 
 export default nextConfig;
+
